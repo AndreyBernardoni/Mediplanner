b77526b4aff80e44ddcd5129268d0b2bf45d827e
Andrey Fialho <fialho.andrey2017@gmail.com>
2023-04-04 15:39:37 -0300

    Bug Fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mediplanner/firebase_options.dart';
 import 'package:mediplanner/repository/authentication_repository.dart';
-import 'package:mediplanner/services/firebase_service.dart';
 import 'package:mediplanner/screens/signup/signup_screen.dart';
 import 'package:mediplanner/screens/add_medication/add_medication_screen.dart';
 
@@ -15,22 +15,18 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  Get.put(
-    AuthenticationRepository(),
-  );
+  Get.put(AuthenticationRepository());
   runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  final FirebaseService firebaseService = FirebaseService.instance;
   MainApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       themeMode: ThemeMode.system,
       debugShowCheckedModeBanner: false,
-      initialRoute: '/signup',
+      initialRoute: '/login',
       getPages: [
         GetPage(
           name: '/signup',
